dba0fbb251e683db63621d1ec262fe97da44b8d9
Bruno Tinti <tinti400@gmail.com>
2025-07-14 16:08:42 -0300

    Ajustando AuthContext e corrigindo exportações

--- src/hooks/route.ts
+++ src/hooks/route.ts
@@ -1,9 +1,15 @@
-// src/hooks/useAuth.ts
-import { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
+'use client'
 
-export function useAuth() {
-  const context = useContext(AuthContext);
-  if (!context) throw new Error("useAuth deve ser usado dentro de AuthProvider");
-  return context;
+import { useAuth } from '../context/AuthContext'
+
+export function useRoute() {
+  const { user } = useAuth()
+
+  if (!user) {
+    if (typeof window !== 'undefined') {
+      window.location.href = '/login'
+    }
+  }
+
+  return user
 }
