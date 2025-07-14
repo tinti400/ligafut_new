1ed48549bdfe233226e83f6c6e083de92c6b505d
Bruno Tinti <tinti400@gmail.com>
2025-07-14 15:47:40 -0300

    Corrigindo supabase.ts para deploy

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,13 +1,6 @@
-// lib/supabase.ts
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
- // padrão se não tiver logo
-    },
-  }));
-
-  return NextResponse.json(formatado);
-}
+export const supabase = createClient(supabaseUrl, supabaseKey)
