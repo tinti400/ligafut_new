3b647dbfa53be37c683b173ee5d65f13c48f25fd
Bruno Tinti <tinti400@gmail.com>
2025-07-26 12:39:23 -0300

    🚀 Projeto LigaFut - Evento de Roubo

--- src/app/admin/leiloes_finalizados/page.tsx
+++ src/app/admin/leiloes_finalizados/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react'
 import { createClient } from '@supabase/supabase-js'
-import { registrarMovimentacao } from '@/utils/utils'
+import { registrarMovimentacao } from '@/utils/registrarMovimentacao'
 
 
 const supabase = createClient(
